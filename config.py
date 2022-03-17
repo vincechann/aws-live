4793c56bb10eeaadfee2ee130ee8d74ef00b373e
vincechann <74403871+vincechann@users.noreply.github.com>
2022-03-17 12:05:09 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cgimzc2nhmtc.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chanmingfoon-employee"
+customregion = "us-east-1"
